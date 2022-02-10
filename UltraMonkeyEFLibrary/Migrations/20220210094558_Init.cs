981216e5e20507a6665a33fbcf0c97ceb02a0515
patrik033 <patrik.odh@gmail.com>
2022-02-10 10:52:57 +0100

    Add project files.

---
+++ UltraMonkeyEFLibrary/Migrations/20220210094558_Init.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UltraMonkeyEFLibrary.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WeatherDatas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    Temp = table.Column<float>(type: "real", nullable: false),
+                    AirMoisture = table.Column<int>(type: "int", nullable: false),
+                    MoldIndex = table.Column<int>(type: "int", nullable: true),
+                    OpenTime = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WeatherDatas", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WeatherDatas");
+        }
+    }
+}
---
+++ UltraMonkeyEFLibrary/UltraMonkeyContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UltraMonkeyEFLibrary
+{
+    public class UltraMonkeyContext : DbContext
+    {
+        public DbSet<WeatherData> WeatherDatas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+            optionsBuilder
+                .UseLoggerFactory(LoggerFactory.Create(builder => builder.AddConsole()))
+                .EnableSensitiveDataLogging()
+                .UseSqlServer(config["ConnectionStrings:DefaultConnection"]);
+        }
+    }
+}
