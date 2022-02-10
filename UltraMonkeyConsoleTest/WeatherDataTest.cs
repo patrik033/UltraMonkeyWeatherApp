a5ebf0fcd8516b44eb835189e39e9bdb53c5b24f
patrik033 <patrik.odh@gmail.com>
2022-02-10 15:21:19 +0100

    fixed the input to database and pushed the data to the database

---
+++ UltraMonkeyConsoleTest/WeatherDataTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UltraMonkeyConsoleTest
+{
+    public class WeatherDataTest
+    {
+        public DateTime Date { get; set; }
+        public string Location { get; set; }
+        public float Temp { get; set; }
+        public int AirMoisture { get; set; }
+        public int? MoldIndex { get; set; }
+        public int? OpenTime { get; set; }
+    }
+}
